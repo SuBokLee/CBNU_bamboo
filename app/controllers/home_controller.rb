2a789022bc0079847b58c2959478bd986eeac0c2
suboklee <smalljot@naver.com>
2018-07-17 20:40:49 +0000

    사용하기추가

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def keyboard_init
     msg = {
       type: "buttons",
-      buttons: ["입력","보기"]
+      buttons: ["사용방법","입력"]
     }
     render json: msg, status: :ok
   end
@@ -12,22 +12,12 @@ class HomeController < ApplicationController
     content = @received_data["content"]
     # @content=params[:content]
     
-    # if content == "입력"
-    #   msg = {
-    #     message: {
-    #       text: "내용을 입력하세요(돌아가기 => 'ㅇㅇ'입력)"
-    #     },
-    #     keyboard: {
-    #       type: "text"
-    #     }
-    #   }
-    #   render json: msg, status: :ok
-      
+
       
     if content == "입력"
       dddd=Post.all
       vv=Post.count
-      gg = "--------------------------------\n최근 글 5개 입니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
+      gg = "--------------------------------\n최근 글 5개 입니다.\n--------------------------------\n" +"- "+ dddd.last.content + "\n\n" +"- "+ dddd.find(vv-1).content + "\n\n" +"- "+ dddd.find(vv-2).content+ "\n\n" +"- "+ dddd.find(vv-3).content + "\n\n" +"- "+ dddd.find(vv-4).content
       
       msg = {
         message: {
@@ -37,28 +27,7 @@ class HomeController < ApplicationController
           type: "text"
         }
       }
-      
-      # msg = {
-      #   message: {
-      #     text: gg,
-      #     message_button: {
-      #       label: "전체보기",
-      #       url:"http://13.209.5.223/home/index"}
-      #   },
-      #   keyboard: {
-      #     type: "text",
-      #   }
-      # }      
-      
-      
-      
-      
       render json: msg, status: :ok      
-      
-      
-      
-      
-      
       
       
       elsif content == "ㅇㅇ"
@@ -68,34 +37,33 @@ class HomeController < ApplicationController
         },
         keyboard: {
           type: "buttons",
-          buttons: ["입력","보기"]
+          buttons: ["사용방법","입력"]
         }
       }
       render json: msg, status: :ok
       
-      elsif content == "보기"
-      # dddd=Post.all
-      # vv=Post.count
-      # gg = "--------------------------------\n최근 글 5개 입니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
-      
+      elsif content == "사용방법"
+
       msg = {
         message: {
-          text: "이것은 미니채팅방입니다.",
+          text: "'입력'을 선택한뒤 원하는 내용을 입력하시면 저장됩니다.\n처음으로 돌아갈때는 'ㅇㅇ'을 입력해주세요.",
+                          photo: {
+                          url: "http://k.kakaocdn.net/dn/DFKfE/btqnm3nWTK6/fR2F7SbUMwBKNKLlme9BN0/img_xl.jpg",
+                          width: 720,
+                          height: 2630},
+                          
           message_button: {
-            label: "전체보기",
-            url:"http://13.209.5.223"}
+            label: "자세한 사용방법",
+            url:"http://pf.kakao.com/_faiYC/23428008"}
         },
         keyboard: {
           type: "buttons",
-          buttons: ["입력","보기"]
+          buttons: ["사용방법","입력"]
         }
       }
       render json: msg, status: :ok      
       
-      
-      
-      
-      
+
     else 
           if @received_data["content"].length < 150
           a=Post.new
@@ -104,14 +72,14 @@ class HomeController < ApplicationController
           
       dddd=Post.all
       vv=Post.count
-      gg = "--------------------------------\n저장되었습니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
+      gg = "--------------------------------\n저장되었습니다.\n--------------------------------\n" +"- "+ dddd.last.content + "\n\n" +"- "+ dddd.find(vv-1).content + "\n\n" +"- "+ dddd.find(vv-2).content+ "\n\n" +"- "+ dddd.find(vv-3).content + "\n\n" +"- "+ dddd.find(vv-4).content
                
           msg = {
             message: {
               text: gg,
               message_button: {
                 label: "전체보기",
-                url:"http://13.209.5.223/home/index"}
+                url:"https://dingco-hackerton-suboklee.c9users.io/home/index"}
             },
             keyboard: {
               type: "text"
@@ -123,10 +91,10 @@ class HomeController < ApplicationController
           
           
           else
-      dddd=Post.all
-      vv=Post.count
-      gg = "--------------------------------\n150자를 넘을 수 없습니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
-               
+          dddd=Post.all
+          vv=Post.count
+          gg = "--------------------------------\n150자를 넘을 수 없습니다.\n--------------------------------\n" +"- "+ dddd.last.content + "\n\n" +"- "+ dddd.find(vv-1).content + "\n\n" +"- "+ dddd.find(vv-2).content+ "\n\n" +"- "+ dddd.find(vv-3).content + "\n\n" +"- "+ dddd.find(vv-4).content
+                   
           msg = {
             message: {
               text: gg,
@@ -141,7 +109,6 @@ class HomeController < ApplicationController
           render json: msg, status: :ok   
           end
     end   #if else end
-    
   end      #keyboard_int end
   
   
