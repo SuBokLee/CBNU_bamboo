b3d3de245227c19c1d401702d15cd9a43c56ab38
suboklee <smalljot@naver.com>
2018-07-17 19:17:28 +0000

    전체적인수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,7 +12,23 @@ class HomeController < ApplicationController
     content = @received_data["content"]
     # @content=params[:content]
     
+    # if content == "입력"
+    #   msg = {
+    #     message: {
+    #       text: "내용을 입력하세요(돌아가기 => 'ㅇㅇ'입력)"
+    #     },
+    #     keyboard: {
+    #       type: "text"
+    #     }
+    #   }
+    #   render json: msg, status: :ok
+      
+      
     if content == "입력"
+      dddd=Post.all
+      vv=Post.count
+      gg = "--------------------------------\n최근 글 5개 입니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
+      
       msg = {
         message: {
           text: "내용을 입력하세요(돌아가기 => 'ㅇㅇ'입력)"
@@ -21,7 +37,29 @@ class HomeController < ApplicationController
           type: "text"
         }
       }
-      render json: msg, status: :ok
+      
+      # msg = {
+      #   message: {
+      #     text: gg,
+      #     message_button: {
+      #       label: "전체보기",
+      #       url:"http://13.209.5.223/home/index"}
+      #   },
+      #   keyboard: {
+      #     type: "text",
+      #   }
+      # }      
+      
+      
+      
+      
+      render json: msg, status: :ok      
+      
+      
+      
+      
+      
+      
       
       elsif content == "ㅇㅇ"
       msg = {
@@ -36,16 +74,16 @@ class HomeController < ApplicationController
       render json: msg, status: :ok
       
       elsif content == "보기"
-      dddd=Post.all
-      vv=Post.count
-      gg = "--------------------------------\n최근 글 5개 입니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
+      # dddd=Post.all
+      # vv=Post.count
+      # gg = "--------------------------------\n최근 글 5개 입니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
       
       msg = {
         message: {
-          text: gg,
+          text: "이것은 미니채팅방입니다.",
           message_button: {
             label: "전체보기",
-            url:"http://13.209.5.223/home/index"}
+            url:"http://13.209.5.223"}
         },
         keyboard: {
           type: "buttons",
@@ -59,32 +97,49 @@ class HomeController < ApplicationController
       
       
     else 
-      if @received_data["content"].length < 150
-      a=Post.new
-      a.content=@received_data["content"]
-      a.save
-      
-      
-      msg = {
-        message: {
-          text: "저장되었습니다."
-        },
-        keyboard: {
-          type: "text"
-        }
-      }
-      render json: msg, status: :ok
-      else
-        msg = {
-          message: {
-            text: "150자를 넘으면 안됩니다."
-          },
-          keyboard: {
-            type: "text"
+          if @received_data["content"].length < 150
+          a=Post.new
+          a.content=@received_data["content"]
+          a.save
+          
+      dddd=Post.all
+      vv=Post.count
+      gg = "--------------------------------\n저장되었습니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
+               
+          msg = {
+            message: {
+              text: gg,
+              message_button: {
+                label: "전체보기",
+                url:"http://13.209.5.223/home/index"}
+            },
+            keyboard: {
+              type: "text"
+            }
           }
-        }
-        render json: msg, status: :ok    
-      end
+          render json: msg, status: :ok 
+          
+          
+          
+          
+          else
+      dddd=Post.all
+      vv=Post.count
+      gg = "--------------------------------\n150자를 넘을 수 없습니다.\n--------------------------------\n" +"1. "+ dddd.last.content + "\n\n" +"2. "+ dddd.find(vv-1).content + "\n\n" +"3. "+ dddd.find(vv-2).content+ "\n\n" +"4. "+ dddd.find(vv-3).content + "\n\n" +"5. "+ dddd.find(vv-4).content
+               
+          msg = {
+            message: {
+              text: gg,
+              message_button: {
+                label: "전체보기",
+                url:"http://13.209.5.223/home/index"}
+            },
+            keyboard: {
+              type: "text",
+            }
+          }
+          render json: msg, status: :ok   
+          end
     end   #if else end
     
   end      #keyboard_int end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'keyboard' => 'home#keyboard_init'
   post 'message' => 'home#keyboard_control'
   # get 'index' => 'home#index'
-  # root 'home#index'
+  root 'home#index'
   
 
   # The priority is based upon order of creation: first created -> highest priority.
